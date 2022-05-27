c202246f7c86a170854177055e8358a333e6bbcd
Edson Rodrigo <edson.silva@ingresse.com>
2022-05-27 04:15:06 -0300

    chore: add logo in center header

---
+++ src/components/Header.tsx
@@ -0,0 +1,156 @@
+import { useRef } from 'react';
+import NextLink from 'next/link';
+import {
+  Flex,
+  Button,
+  HStack,
+  Image,
+  Link,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerBody,
+  DrawerHeader,
+  DrawerContent,
+  DrawerCloseButton,
+  Stack,
+  Box,
+} from '@chakra-ui/react';
+import { useTranslation } from 'next-i18next';
+import { FiMenu, FiX } from 'react-icons/fi';
+
+import type { FlexProps, StackProps } from '@chakra-ui/react';
+import type { WithTranslation } from 'next-i18next';
+import type { NextPage } from 'next';
+
+const DesktopNav = ({
+  t,
+  direction = 'row',
+  spacing = 20,
+  ...rest
+}: Omit<WithTranslation, 'i18n' | 'tReady'> & StackProps) => {
+  return (
+    <Stack
+      direction={direction}
+      as="nav"
+      spacing={spacing}
+      color="haipe.orange.700"
+      fontWeight="medium"
+      align="start"
+      {...rest}
+    >
+      <NextLink href="/como-funciona" passHref>
+        <Link>{t('how-it-works')}</Link>
+      </NextLink>
+      <NextLink href="/planos" passHref>
+        <Link>{t('plans')}</Link>
+      </NextLink>
+      <NextLink href="/contato" passHref>
+        <Link>{t('contact')}</Link>
+      </NextLink>
+    </Stack>
+  );
+};
+
+const Logo = () => {
+  return (
+    <NextLink href="/" passHref>
+      <Link>
+        <Image src="/assets/logo.png" alt="Haipe" />
+      </Link>
+    </NextLink>
+  );
+};
+
+const ButtonLogin = ({ t }: Omit<WithTranslation, 'i18n' | 'tReady'>) => {
+  return (
+    <Flex>
+      <Button
+        color="haipe.orange.800"
+        bgColor="white"
+        fontSize="sm"
+        fontWeight="medium"
+        borderColor="haipe.gray.300"
+        variant="outline"
+      >
+        {t('i-dont-have-an-account-yet')}
+      </Button>
+    </Flex>
+  );
+};
+
+export const HeaderOld: NextPage<FlexProps> = ({ w, maxW, px }) => {
+  const { t } = useTranslation('header');
+
+  const { isOpen, onToggle, onClose } = useDisclosure();
+  const btnRef = useRef<any>();
+
+  return (
+    <>
+      <Flex
+        as="section"
+        w={w}
+        h="20"
+        borderBottom="1px"
+        borderColor="haipe.gray.300"
+        align="center"
+        justifyContent="center"
+      >
+        <HStack
+          as="header"
+          w="full"
+          maxW={maxW}
+          px={px}
+          align="center"
+          justifyContent="space-between"
+        >
+          <IconButton
+            ref={btnRef}
+            display={{ base: 'flex', md: 'none' }}
+            onClick={onToggle}
+            icon={isOpen ? <FiX size={30} /> : <FiMenu size={30} />}
+            variant="ghost"
+            aria-label="Nav of the Haipe"
+          />
+
+          <Logo />
+          <Box w="4" />
+
+          <Flex display={{ base: 'none', md: 'flex' }}>
+            <DesktopNav t={t} />
+          </Flex>
+
+          <Flex display={{ base: 'none', md: 'flex' }}>
+            <ButtonLogin t={t} />
+          </Flex>
+        </HStack>
+      </Flex>
+
+      <Drawer
+        placement="top"
+        onClose={onClose}
+        isOpen={isOpen}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent bg="haipe.gray.100">
+          <DrawerCloseButton />
+          <DrawerHeader
+            as={Flex}
+            borderBottom="1px"
+            borderColor="haipe.gray.300"
+          >
+            <Logo />
+          </DrawerHeader>
+          <DrawerBody pb={6}>
+            <DesktopNav t={t} direction="column" spacing={2} mt={2} mb={4} />
+            <ButtonLogin t={t} />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default HeaderOld;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,11 @@
-import { Button, FlexProps, HStack, Image, Link } from '@chakra-ui/react';
-import { Box, Flex } from '@chakra-ui/react';
-import { useTranslation } from 'next-i18next';
-import NextLink from 'next/link';
+import { Flex } from '@chakra-ui/react';
 
 import type { NextPage } from 'next';
+import type { FlexProps } from '@chakra-ui/react';
 
 import SEO from '@/components/SEO';
 import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 const Layout: NextPage<FlexProps> = ({
   children,
@@ -17,8 +16,6 @@ const Layout: NextPage<FlexProps> = ({
   px = '3',
   ...rest
 }) => {
-  const { t } = useTranslation('header');
-
   return (
     <Flex
       direction="column"
@@ -27,60 +24,8 @@ const Layout: NextPage<FlexProps> = ({
       minH="100vh"
     >
       <SEO title="Haipe" shouldExcludeTitleSuffix />
-      <Flex
-        as="section"
-        w={w}
-        h="20"
-        borderBottom="1px"
-        borderColor="haipe.gray.300"
-        align="center"
-        justifyContent="center"
-      >
-        <HStack
-          as="header"
-          w="full"
-          maxW={maxW}
-          px={px}
-          align="center"
-          justifyContent="space-between"
-        >
-          <NextLink href="/" passHref>
-            <Link>
-              <Image src="/assets/logo.png" alt="Haipe" />
-            </Link>
-          </NextLink>
 
-          <HStack
-            as="nav"
-            spacing="20"
-            color="haipe.orange.700"
-            fontWeight="medium"
-          >
-            <NextLink href="/como-funciona" passHref>
-              <Link>{t('how-it-works')}</Link>
-            </NextLink>
-            <NextLink href="/planos" passHref>
-              <Link>{t('plans')}</Link>
-            </NextLink>
-            <NextLink href="/contato" passHref>
-              <Link>{t('contact')}</Link>
-            </NextLink>
-          </HStack>
-
-          <Box>
-            <Button
-              color="haipe.orange.800"
-              bgColor="white"
-              fontSize="sm"
-              fontWeight="medium"
-              borderColor="haipe.gray.300"
-              variant="outline"
-            >
-              {t('i-dont-have-an-account-yet')}
-            </Button>
-          </Box>
-        </HStack>
-      </Flex>
+      <Header maxW={maxW} w={w} px={px} />
 
       <Flex as="main" flex={flex} w={w} maxW={maxW} py={py} px={px} {...rest}>
         {children}
